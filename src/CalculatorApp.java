892dd8a000e826a9db20bdd30214157d6fff5469
Luka Uršič <urlu@pm.me>
2023-01-03 13:48:39 +0100

    Initial commit
    Look and feel done

---
+++ src/CalculatorApp.java
@@ -0,0 +1,159 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+public class CalculatorApp extends JFrame {
+
+    private Border debugBorder = BorderFactory.createLineBorder(Color.GREEN, 5);
+    private Color orange = new Color(255, 149, 12),
+            darkGray = new Color(70, 60, 60),
+            midGray = new Color(92, 92, 92),
+            lightGray = new Color(113, 107, 108);
+    private JLabel text = new JLabel("0");
+    private CalculatorButton but_ac, but_negative, but_mod, but_divide,
+                    but7, but8, but9, but_multiply,
+                    but4, but5, but6, but_subtract,
+                    but1, but2, but3, but_sum,
+                    but0, but_float, but_equals;
+
+    public CalculatorApp() {
+        prepareGUI();
+    }
+
+    public void prepareGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Calculator");
+        setBackground(darkGray);
+        setSize(230, 320);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("calc_icon.png"); // TODO: se ne dela
+        setIconImage(icon.getImage());
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(new Color(83, 83, 83));
+
+        text.setHorizontalAlignment(JLabel.RIGHT);
+        text.setForeground(Color.WHITE);
+        text.setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 20));
+        text.setFont(new Font("Arial", Font.PLAIN, 50));
+        mainPanel.add(text, BorderLayout.NORTH);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        JPanel buttonsPanel = new JPanel(new GridBagLayout());
+        buttonsPanel.setBackground(darkGray);
+
+
+        but_ac = new CalculatorButton("AC", midGray);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        buttonsPanel.add(but_ac, gbc);
+
+        but_negative = new CalculatorButton("+/-", midGray);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        buttonsPanel.add(but_negative, gbc);
+
+        but_mod = new CalculatorButton("%", midGray);
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        buttonsPanel.add(but_mod, gbc);
+
+        but_divide = new CalculatorButton("/", orange);
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        buttonsPanel.add(but_divide, gbc);
+
+
+        but7 = new CalculatorButton("7", lightGray);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        buttonsPanel.add(but7, gbc);
+
+        but8 = new CalculatorButton("8", lightGray);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        buttonsPanel.add(but8, gbc);
+
+        but9 = new CalculatorButton("9", lightGray);
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        buttonsPanel.add(but9, gbc);
+
+        but_multiply = new CalculatorButton("*", orange);
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        buttonsPanel.add(but_multiply, gbc);
+
+
+        but4 = new CalculatorButton("4", lightGray);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        buttonsPanel.add(but4, gbc);
+
+        but5 = new CalculatorButton("5", lightGray);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        buttonsPanel.add(but5, gbc);
+
+        but6 = new CalculatorButton("6", lightGray);
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        buttonsPanel.add(but6, gbc);
+
+        but_subtract = new CalculatorButton("-", orange);
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        buttonsPanel.add(but_subtract, gbc);
+
+
+        but1 = new CalculatorButton("1", lightGray);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        buttonsPanel.add(but1, gbc);
+
+        but2 = new CalculatorButton("2", lightGray);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        buttonsPanel.add(but2, gbc);
+
+        but3 = new CalculatorButton("3", lightGray);
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        buttonsPanel.add(but3, gbc);
+
+        but_sum = new CalculatorButton("+", orange);
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        buttonsPanel.add(but_sum, gbc);
+
+
+        but0 = new CalculatorButton("0", lightGray);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        // gbc.fill = GridBagConstraints.HORIZONTAL;
+        buttonsPanel.add(but0, gbc);
+
+        but_float = new CalculatorButton(",", lightGray);
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        gbc.gridwidth = 1;
+        buttonsPanel.add(but_float, gbc);
+
+        but_equals = new CalculatorButton("=", orange);
+        gbc.gridx = 3;
+        gbc.gridy = 4;
+        buttonsPanel.add(but_equals, gbc);
+
+
+        mainPanel.add(buttonsPanel);
+        this.add(mainPanel);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CalculatorApp();
+    }
+}
---
+++ src/CalculatorButton.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class CalculatorButton extends JButton {
+    public CalculatorButton(String text, Color bg_color) {
+        this.setFocusPainted(false);
+        this.setText(text);
+        this.setOpaque(true);
+        this.setBackground(bg_color);
+        this.setForeground(Color.WHITE);
+
+        Border outside_border = BorderFactory.createMatteBorder(1, 1, 0, 0, new Color(73, 73, 73));
+        Border inside_border = (BorderFactory.createEmptyBorder(15, 20, 15, 20));
+        this.setBorder(BorderFactory.createCompoundBorder(outside_border, inside_border));
+
+        this.setFont(new Font("Xenara", Font.BOLD, 16));
+    }
+
+    public void changeColorTo(Color color) {
+        this.setBackground(color);
+    }
+
+    public void changeTextTo(String text) {
+        this.setText(text);
+    }
+}
