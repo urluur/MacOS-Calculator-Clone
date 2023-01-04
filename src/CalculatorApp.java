cc6d1dcd8a1c134a85d367ddf88d24e7dbd6ca13
Luka Uršič <urlu@pm.me>
2023-01-04 18:55:47 +0100

    all functionality done
    not polished but everything is working as expected

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -1,24 +1,19 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CalculatorApp extends JFrame {
-
-    private final Border debugBorder = BorderFactory.createLineBorder(Color.GREEN, 5);
-
     private static final Color orange = new Color(255, 149, 12),
             darkGray = new Color(80, 75, 75),
             midGray = new Color(91, 91, 91),
             lightGray = new Color(113, 107, 108);
-    private JLabel text = new JLabel("0");
+    private final JLabel text = new JLabel("0");
     private CalculatorButton but_ac, but_negative, but_mod, but_divide,
                     but7, but8, but9, but_multiply,
                     but4, but5, but6, but_subtract,
-                    but1, but2, but3, but_sum,
+                    but1, but2, but3, but_add,
                     but0, but_float, but_equals;
 
+    private String arg1 = null, arg2 = null, operator = null;
 
     public CalculatorApp() {
         prepareGUI();
@@ -128,10 +123,10 @@ public class CalculatorApp extends JFrame {
         gbc.gridy = 3;
         buttonsPanel.add(but3, gbc);
 
-        but_sum = new CalculatorButton("+", orange);
+        but_add = new CalculatorButton("+", orange);
         gbc.gridx = 3;
         gbc.gridy = 3;
-        buttonsPanel.add(but_sum, gbc);
+        buttonsPanel.add(but_add, gbc);
 
 
         but0 = new CalculatorButton("0", lightGray);
@@ -154,152 +149,192 @@ public class CalculatorApp extends JFrame {
 
         mainPanel.add(buttonsPanel);
         this.add(mainPanel);
-        // this.setUndecorated(true); // TODO: custom window decorations
+//        this.setUndecorated(true); // TODO: custom window decorations
         this.pack();
         this.setVisible(true);
     }
 
     public void addButtonListeners() {
-        but_ac.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO: start working here
+        but_ac.addActionListener(e -> {
+            if (oneArgNoOperator()) {
+                arg1 = null;
+                changeToAC();
+                text.setText("0");
+            } else if (oneArgAndOperator()) {
+                operator = null;
+                changeToAC();
+            } else if (bothArgsAndOperator()) {
+                arg2 = null;
+                changeToAC();
+                text.setText("0");
             }
         });
 
-        but_negative.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+        but_negative.addActionListener(e -> {
+            if (oneArgNoOperator() || oneArgAndOperator()) {
+                arg1 = resolveNegative(arg1);
+                text.setText(arg1);
+            } else if (bothArgsAndOperator()) {
+                arg2 = resolveNegative(arg2);
+                text.setText(arg2);
             }
         });
 
-        but_mod.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        but_mod.addActionListener(e -> resolveOperator("%"));
 
+        but_divide.addActionListener(e -> resolveOperator("/"));
+
+        but7.addActionListener(e -> pressed("7"));
+
+        but8.addActionListener(e -> pressed("8"));
+
+        but9.addActionListener(e -> pressed("9"));
+
+        but_multiply.addActionListener(e -> resolveOperator("*"));
+
+        but4.addActionListener(e -> pressed("4"));
+
+        but5.addActionListener(e -> pressed("5"));
+
+        but6.addActionListener(e -> pressed("6"));
+
+        but_subtract.addActionListener(e -> resolveOperator("-"));
+
+        but1.addActionListener(e -> pressed("1"));
+
+        but2.addActionListener(e -> pressed("2"));
+
+        but3.addActionListener(e -> pressed("3"));
+
+        but_add.addActionListener(e -> resolveOperator("+"));
+
+        but0.addActionListener(e -> {
+            if (oneArgNoOperator()) {
+                arg1 = arg1.concat("0");
+                text.setText(arg1);
+            } else if (bothArgsAndOperator()) {
+                arg2 = arg2.concat("0");
+                text.setText(arg2);
             }
         });
 
-        but_divide.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+        but_float.addActionListener(e -> {
+            if (noArgNoOperator()) {
+                arg1 = "0.";
+            } else if (oneArgNoOperator()) {
+                if (!arg1.contains(".")) {
+                    arg1 = arg1.concat(".");
+                    text.setText(arg1);
+                }
+            } else if (oneArgAndOperator()) {
+                arg2 = "0.";
+                text.setText(arg2);
+            } else if (bothArgsAndOperator()) {
+                if(!arg2.contains(".")) {
+                    arg2 = arg2.concat(".");
+                    text.setText(arg2);
+                }
             }
         });
 
-        but7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+        but_equals.addActionListener(e -> {
+            if (oneArgAndOperator()) {
+                arg2 = arg1;
+                arg1 = solve(arg1, operator, arg2);
+                operator = null;
+                arg2 = null;
+                fSetText(arg1);
+            } else if (bothArgsAndOperator()) {
+                arg1 = solve(arg1, operator, arg2);
+                operator = null;
+                arg2 = null;
+                fSetText(arg1);
             }
         });
-
-        but8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but9.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but_multiply.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but6.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but_subtract.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but_sum.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but0.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but_float.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        but_equals.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
     }
 
-    public String noDoubleZero(String in) {
-        if (in.endsWith(".0")) {
-            return in.substring(0, in.indexOf("."));
+    public String solve(String arg1, String operator, String arg2) {
+        double x = Double.parseDouble(arg1), y = Double.parseDouble(arg2), result = 0.0;
+        switch (operator) {
+            case "+" -> result = x + y;
+            case "-" -> result = x - y;
+            case "*" -> result = x * y;
+            case "/" -> result = x / y;
+            case "%" -> result = x % y;
+            default -> System.out.println("Error: invalid operator.");
         }
-        else return in;
+        return result + "";
+    }
+
+    public void resolveOperator(String symbol) {
+        if (noArgNoOperator()) {
+            arg1 = "0";
+            operator = symbol;
+        } else if (oneArgNoOperator()) {
+            operator = symbol;
+        } else if (oneArgAndOperator()) {
+            operator = symbol;
+        } else if (bothArgsAndOperator()) {
+            arg1 = solve(arg1, operator, arg2);
+            operator = symbol;
+            arg2 = null;
+            fSetText(arg1);
+        }
+    }
+
+    public boolean noArgNoOperator() {
+        return arg1 == null && operator == null && arg2 == null;
+    }
+
+    public boolean oneArgNoOperator() {
+        return arg1 != null && operator == null && arg2 == null;
+    }
+
+    public boolean oneArgAndOperator() {
+        return arg1 != null && operator != null && arg2 == null;
+    }
+
+    public boolean bothArgsAndOperator() {
+        return arg1 != null && operator != null && arg2 != null;
+    }
+
+    public String resolveNegative(String x) {
+        if (x.startsWith("-")) {
+            return x.substring(1);
+        }
+        else {
+            return "-" + x;
+        }
+    }
+
+    public void pressed(String fresh) {
+        changeToC();
+        if (noArgNoOperator()) {
+            arg1 = fresh;
+            text.setText(arg1);
+        } else if (oneArgNoOperator()) {
+            arg1 = arg1.concat(fresh);
+            text.setText(arg1);
+        } else if (oneArgAndOperator()) {
+            arg2 = fresh;
+            text.setText(arg2);
+        } else if (bothArgsAndOperator()) {
+            arg2 = arg2.concat(fresh);
+            text.setText(arg2);
+        }
+    }
+
+    public void changeToC() {
+        but_ac.changeTextTo(" C "); // TODO: without whitespaces
+    }
+
+    public void changeToAC() {
+        but_ac.changeTextTo("AC");
+    }
+
+    public void fSetText(String fresh) {
+        // TODO: print doubles without 0.0
+        text.setText(fresh);
     }
 
     public static Color getOrange() {
